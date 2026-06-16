2a0e3095c35a5c028afadcb14e980acbf794689e
leah <elss4@aluno.ifal.edu.br>
2026-06-15 23:40:55 -0300

    Delete tests directory

--- tests/index.js
+++
@@ -1,5 +0,0 @@
-export { UsuarioRepository } from './UsuarioRepository.js';
-export { FilmeRepository } from './FilmeRepository.js';
-export { SerieRepository } from './SerieRepository.js';
-export { ComentarioRepository } from './ComentarioRepository.js';
-export { ListaAssistirRepository } from './ListaAssistirRepository.js';
