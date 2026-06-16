6ef7292605e9dee1980b7b5ee91a45364c81f0c4
leah <elss4@aluno.ifal.edu.br>
2026-06-15 23:40:36 -0300

    Delete src directory

--- src/models/index.js
+++
@@ -1,5 +0,0 @@
-export { Usuario, UsuarioComum, UsuarioAdmin } from './Usuario.js';
-export { Filme } from './Filme.js';
-export { Serie } from './Serie.js';
-export { Avaliacao } from './Avaliacao.js';
-export { ListaAssistir } from './ListaAssistir.js';
